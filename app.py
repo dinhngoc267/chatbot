f460f165ad13c1a54c5ede5ea178d9a9301af974
Lê Đình Ngọc <dinhngoc123@gmail.com>
2021-05-29 19:42:34 +0700

    Init chatbot

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__,
+            static_folder='static')
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+    app.run(debug=True)
